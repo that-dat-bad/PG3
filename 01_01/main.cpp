6109d376af4da7e5e516f7e1968842fe2c28f401
Takanaga Daiki <k024g0112@g.neec.ac.jp>
2025-09-29 12:05:00 +0900

    01_01を完了

---
+++ 01_01/main.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<list>
+#include<string>
+
+int main() {
+	std::list<std::string> YamanoteLine = { "Tokyo","Kanda","Akihabara","Okachimachi","Ueno",
+		"Uguisudani","Nippori","Tabata","Komagome",
+		"Sugamo","Otsuka","Ikebukuro","Mejiro","Takadanobaba",
+		"Shin-Okubo","Shinjuku","Yoyogi","Harajuku","Shibuya",
+		"Ebisu","Meguro","Gotanda","Osaki","Shinagawa",
+		"Tamachi","Hamamatsucho","Shimbashi","Yurakucho" };
+
+	std::cout << "in 1970" << std::endl;
+	for (auto itr = YamanoteLine.begin(); itr != YamanoteLine.end(); ++itr) {
+		std::cout << *itr;
+		if (std::next(itr) != YamanoteLine.end()) {
+			std::cout << ",";
+		}
+	}
+	std::cout << std::endl;
+
+
+	for (auto itr = YamanoteLine.begin(); itr != YamanoteLine.end(); ++itr) {
+		if (*itr == "Tabata") {
+			YamanoteLine.insert(itr, "Nishi-Nippori");
+			break;
+		}
+	}
+
+	std::cout << "\nin 2019" << std::endl;
+	for (auto itr = YamanoteLine.begin(); itr != YamanoteLine.end(); ++itr) {
+		std::cout << *itr;
+		if (std::next(itr) != YamanoteLine.end()) {
+			std::cout << ",";
+		}
+	}
+	std::cout << std::endl;
+
+	for (auto itr = YamanoteLine.begin(); itr != YamanoteLine.end(); ++itr) {
+		if (*itr == "Tamachi") {
+			YamanoteLine.insert(itr, "Takanawa Gateway");
+			break;
+		}
+	}
+
+	std::cout << "\nin 2022" << std::endl;
+	for (auto itr = YamanoteLine.begin(); itr != YamanoteLine.end(); ++itr) {
+		std::cout << *itr;
+		if (std::next(itr) != YamanoteLine.end()) {
+			std::cout << ",";
+		}
+	}
+
+	return 0;
+}
