9bc0a3aebc231e4510c7bdf8b92c5d3842d4144e
Takanaga Daiki <k024g0112@g.neec.ac.jp>
2025-09-29 11:04:32 +0900

    00_01の課題を完了

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,10 +1,8 @@
 #include<iostream>
-#include<windows.h>
 
 int main() {
-	SetConsoleOutputCP(65001);
-	std::cout << "Hello, PG3!" << std::endl;
-	std::cout << "こんにちは、PG3！" << std::endl;
+	char character[] = "ソ";
+	printf("%s", character);
 
 	return 0;
 
