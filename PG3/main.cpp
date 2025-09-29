c0ffd8140229f188532e298190ea81522053dcd4
Takanaga Daiki <k024g0112@g.neec.ac.jp>
2025-09-29 10:45:39 +0900

    プロジェクト ファイルを追加します。

---
+++ PG3/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<windows.h>
+
+int main() {
+	SetConsoleOutputCP(65001);
+	std::cout << "Hello, PG3!" << std::endl;
+	std::cout << "こんにちは、PG3！" << std::endl;
+
+	return 0;
+
+}
