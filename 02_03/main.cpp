9108dd24803908beec94414588452c3942c19ebd
Takanaga Daiki <k024g0112@g.neec.ac.jp>
2025-11-17 10:11:44 +0900

    02_03を完了

--- 02_03/main.cpp
+++ 02_03/main.cpp
@@ -1,23 +1,29 @@
 #include<iostream>
 #include<Windows.h>
+#include<random>
 
 
-typedef void (*PFunc)(int*);
 
-void DispResult(int *s){
-	std::cout << "Result: " << *s << std::endl;
+void ShowResult(int roll, int userGuess) {
+	std::cout << "出目は " << roll + 1 << " です。" << std::endl;
+	if ((roll % 2 == 0 && userGuess == 1) || (roll % 2 == 1 && userGuess == 0)) {
+
+		std::cout << "あなたの勝ちです！" << std::endl;
+	} else {
+		std::cout << "あなたの負けです！" << std::endl;
+	}
 }
 
-void SetTimeOut(PFunc p, int second) {
-	Sleep(second * 1000);
+void DelayReveal(void (*fn)(int, int), unsigned int delayMs, int roll, int userGuess) {
+	Sleep(delayMs);
+	ShowResult(roll, userGuess);
 }
 
 int main() {
-	std::cout << "Start" << std::endl;
-
-	PFunc p;
-	p = DispResult;
-	SetTimeOut(p, 5);
-
+	std::cout << "半か丁か(半:1,丁:0と入力)" << std::endl;
+	int userChoice = scanf_s("%d", &userChoice);
+	srand((unsigned)time(NULL));
+	int roll = rand() % 6;
+	DelayReveal(ShowResult, 3000, roll, userChoice);
 	return 0;
 }
